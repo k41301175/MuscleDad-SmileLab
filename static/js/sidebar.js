bbeed17ecfd3b9f587e05a541b59a93e27c1cbb4
soda_kunita <kunita@nico-soda.jp>
2025-04-11 14:51:50 +0900

    Adjust screen for smartphone

---
+++ static/js/sidebar.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  const menu = document.getElementById('mobileMenu');
+  if (menu.style.display === 'flex') {
+    menu.style.display = 'none';
+  } else {
+    menu.style.display = 'flex';
+  }
+}
